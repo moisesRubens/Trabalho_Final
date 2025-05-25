1284ca9cbddce6c00613484a0a04374e86a94812
moisesRubens <moisesrubens.pro@gmail.com>
2025-05-25 20:20:12 -0300

    Testando

---
+++ src/main/java/com/mycompany/projeto_final/Projeto_Final.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_final;
+
+/**
+ *
+ * @author moise
+ */
+public class Projeto_Final {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
